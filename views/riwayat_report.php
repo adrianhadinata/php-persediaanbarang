542fe214abf5fa683f1cb0b2fb742cf6549b35a7
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2023-12-13 06:39:31 +0700

    Fix: Riwayat Repor.php

--- views/riwayat_report.php
+++ views/riwayat_report.php
@@ -9,7 +9,15 @@
                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
                         <thead>
                             <tr>
-                                <th>No</th><th width="17%">Kode</th><th>Nama Customer</th><th>Nama Barang</th><th>Tanggal Pesan</th><th>Tanggal Pengemasan</th><th>Jumlah Barang</th><th>Estimasi Pengiriman</th><th>AKSI</th>
+                                <th>No</th>
+                                <th width="17%">Kode</th>
+                                <th>Nama Customer</th>
+                                <th>Nama Barang</th>
+                                <th>Tanggal Pesan</th>
+                                <th>Tanggal Pengemasan</th>
+                                <th>Jumlah Barang</th>
+                                <th>Estimasi Pengiriman</th>
+                                <th>AKSI</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -26,16 +34,16 @@
                             //Melakukan perulangan u/menampilkan data
                             while ($data = mysqli_fetch_array($query)) {
                                 $nomor++; //Penambahan satu untuk nilai var nomor
-                                ?>
+                            ?>
                                 <tr>
                                     <td><?= $nomor ?></td>
-									<td><?= $data['kode'] ?></td>
+                                    <td><?= $data['kode'] ?></td>
                                     <td><?= $data['nama_customer'] ?></td>
                                     <td><?= $data['nama_barang'] ?></td>
                                     <td><?= $data['tgl_pesan'] ?></td>
                                     <td><?= $data['tgl_pengemasan'] ?></td>
                                     <td><?= $data['jumlah_barang'] ?> unit</td>
-									<td><?= $data['estimasi_pengiriman'] ?> hari</td>
+                                    <td><?= $data['dikirim'] ?> hari</td>
                                     <td>
                                         <a href="report/riwayat_satu.php?id=<?= $data['id'] ?>" target="_blank" class="btn btn-info btn-xs">
                                             <span class="fa fa-print"></span>
@@ -76,32 +84,32 @@
     <div>
         <a href="#" title="Close" class="close">X</a>
 
-        <form  target="_blank" action="report/riwayat_perbulan.php" method="post">
-        <h4>Pilih bulan </h4>
-        <select name="bulan" class="form-control">
-          <option value="12"> Desember </option>
-          <option value="11"> November </option>
-          <option value="10"> Oktober </option>
-          <option value="09"> September </option>
-          <option value="08"> Agustus </option>
-          <option value="07"> Juli </option>
-          <option value="06"> Juni </option>
-          <option value="05"> Mei </option>
-          <option value="04"> April </option>
-          <option value="03"> Maret </option>
-          <option value="02"> Februari </option>
-          <option value="01"> Januari </option>
-        </select>
-        <h4>Pilih tahun</h4>
-        <select name="tahun" class="form-control">
-          <?php
-            for ($i=substr(date("d-m-Y"),6,4); $i > substr(date("d-m-Y"),6,4)-5; $i--) { ?>
-              <option value="<?=$i?>"> <?=$i?> </option>
-          <?php }
-          ?>
-        </select>
+        <form target="_blank" action="report/riwayat_perbulan.php" method="post">
+            <h4>Pilih bulan </h4>
+            <select name="bulan" class="form-control">
+                <option value="12"> Desember </option>
+                <option value="11"> November </option>
+                <option value="10"> Oktober </option>
+                <option value="09"> September </option>
+                <option value="08"> Agustus </option>
+                <option value="07"> Juli </option>
+                <option value="06"> Juni </option>
+                <option value="05"> Mei </option>
+                <option value="04"> April </option>
+                <option value="03"> Maret </option>
+                <option value="02"> Februari </option>
+                <option value="01"> Januari </option>
+            </select>
+            <h4>Pilih tahun</h4>
+            <select name="tahun" class="form-control">
+                <?php
+                for ($i = substr(date("d-m-Y"), 6, 4); $i > substr(date("d-m-Y"), 6, 4) - 5; $i--) { ?>
+                    <option value="<?= $i ?>"> <?= $i ?> </option>
+                <?php }
+                ?>
+            </select>
 
-        <button type="submit">OK</button>
+            <button type="submit">OK</button>
         </form>
     </div>
 </div>
@@ -110,17 +118,17 @@
     <div>
         <a href="#" title="Close" class="close">X</a>
 
-        <form  target="_blank" action="report/riwayat_pertahun.php" method="post">
-        <h4>Pilih tahun</h4>
-        <select name="tahun" class="form-control">
-          <?php
-            for ($i=substr(date("d-m-Y"),6,4); $i > substr(date("d-m-Y"),6,4)-5; $i--) { ?>
-              <option value="<?=$i?>"> <?=$i?> </option>
-          <?php  }
-          ?>
-        </select>
+        <form target="_blank" action="report/riwayat_pertahun.php" method="post">
+            <h4>Pilih tahun</h4>
+            <select name="tahun" class="form-control">
+                <?php
+                for ($i = substr(date("d-m-Y"), 6, 4); $i > substr(date("d-m-Y"), 6, 4) - 5; $i--) { ?>
+                    <option value="<?= $i ?>"> <?= $i ?> </option>
+                <?php  }
+                ?>
+            </select>
 
-        <button type="submit">OK</button>
+            <button type="submit">OK</button>
         </form>
     </div>
 </div>
